7131ce6c4a075ee483340b9a47e5f9a77c76b605
somesh1611 <gawadesomesh402@gmail.com>
2020-11-30 20:24:10 +0530

    Authentication Done.

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,33 +1,52 @@
 package com.example.demo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button logout;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView tvSignUp = findViewById(R.id.tvSignUp);
-        TextView tvSignUp2 = findViewById(R.id.tvSignUp2);
-        tvSignUp.setOnClickListener(onClickSignUp());
-        tvSignUp2.setOnClickListener(onClickSignUp());
-
-    }
-
-    private View.OnClickListener onClickSignUp(){
-        return new View.OnClickListener() {
+        // This function will execute when the user selects a logout button
+        logout=findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,signup.class);
-                MainActivity.this.startActivity(intent);
+
+                // Sign out the user
+                firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
             }
-        };
+        });
     }
+
+
 }
